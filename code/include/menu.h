3d41d4ff6afee906e670449ac9b13979dd4f4448
DA SILVA Yoann <ydasilva@cyclops.pedago.ipb.fr>
2022-03-02 09:03:01 +0100

    debut de menu.h

---
+++ code/include/menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+/**
+ * @brief Choose to play a game or quit the prog
+ * 
+ */
+void general(); 
+
+/**
+ * @brief Choose game mode, taille grille, nb mines ...}
+ * 
+ * @return int 
+ */
+int play_game();
+
+
+#endif /*MENU_H*/
