dab192a2ed20e14e5ae62815722233603469aca1
DA SILVA Yoann <ydasilva@goudurix.pedago.ipb.fr>
2022-03-09 18:11:05 +0100

    MenuV2

--- code/src/menu.c
+++ code/src/menu.c
@@ -2,32 +2,60 @@
 #include <stdlib.h>
 #include<menu.h>
 #include <ctype.h> 
+#include <unistd.h> // to use sleep for debug
 
+//https://stackoverflow.com/questions/49460307/using-fgets-and-strtol-to-get-a-single-integer
+//https://openclassrooms.com/forum/sujet/equivalent-de-fgets-pour-les-int
 int config_game(){
     printf("CONFIG GAME OK\n");
     return 0;
 }
 
-void welcome(){
-    printf("*************************\n");
-    printf("****** 1.play ******\n");
-    printf("****** 0.exit ******\n");
-    printf("*************************\n");
-    printf(" Please select a :>");
-    int choice = 2;
-    scanf("%d", &choice);
-    if (choice == 1){
-    config_game();
-    } else if (choice == 0) {
-        printf("Bye!\n");
-    } else {
-        printf("Input error, please retry!");
-        welcome();
+void printWelcome() {
+    printf("======================\n");
+    printf("****** 1. Play  ******\n");
+    printf("****** 0. Exit  ******\n");
+    printf("======================\n");
+}
+
+void welcome(int hasError){
+    //printf("%d\n",hasError);
+    if(hasError < 2)
+        printWelcome();
+    printf("\nPlease select a choice :>");
+    int choice = 5;
+    int scanf_return = scanf("%d", &choice);
+    //atoi(choice); //test
+    //printf("choice=%d\n", choice); //for debug var
+    //printf("scanf_returned:%d\n", scanf_return); //for debug var
+    //sleep(1);
+
+    if (scanf_return == 1){
+        if (choice == 1){
+            config_game();
+        } else if (choice == 0) {
+            printf("Bye!\n");
+        } else {
+            if (hasError < 1)
+                printf("\nPlease select a valid integer between 0 and 1!\n");
+            welcome(hasError+1);
+        }
+    } else { //if str
+        if (hasError < 1){
+            printf("\nPlease enter a valid integer between 0 and 1!\n");
+            printWelcome();
+        }
+        do {
+            choice = getchar();
+        }
+        while (!isdigit(choice) || choice=='\0');
+        ungetc(choice,stdin);
+        welcome(hasError+2);
     }
 }
 
 int main(int argc, char const *argv[]){
-    welcome();
+    welcome(0);
     return 0;
 }
 
