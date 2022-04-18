eb8b49e1c5705cd49fdcd94963ce8be161241181
Jérémy Grelier <jeremy.grelier33@enseirb-matmeca.fr>
2022-04-18 13:46:50 +0200

    EXTERN

--- code/src/menu.c
+++ code/src/menu.c
@@ -7,7 +7,7 @@
 #include "../include/grid.h"
 
 //Global vars : AJOUTER extern UNE FOIS LE PROG FONCTIONNEL! 
-unsigned int game_mode, auto_game_mode, nb_games, 
+extern unsigned int game_mode, auto_game_mode, nb_games, 
     grid_length, grid_width, mine_number;
 
 void viderBuffer(){
